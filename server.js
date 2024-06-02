565d00e4c4852ee8d90b528bf1388ce8b2af323c
Peter <pqiu85674@gmail.com>
2024-06-02 16:24:09 +0800

    部份修改

--- server.js
+++ server.js
@@ -10,62 +10,55 @@
   });
 }); */
 
-
-const http = require('http');
-const { v4: uuidv4 } = require('uuid');
-const { errorHandle, successHandle } = require('./Handle');
+const http = require("http");
+const { v4: uuidv4 } = require("uuid");
+const { errorHandle, successHandle } = require("./Handle");
 const headers = {
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-  'Content-Type': 'application/json'
+  "Access-Control-Allow-Headers":
+    "Content-Type, Authorization, Content-Length, X-Requested-With",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "PATCH, POST, GET,OPTIONS,DELETE",
+  "Content-Type": "application/json",
 };
 
 let todos = [];
 
 const requestListener = (req, res) => {
   let body = "";
-  req.on('data', chunk => {
+  req.on("data", (chunk) => {
     body += chunk;
-  })
-
+  });
 
   if (req.method == "GET" && req.url == "/todo") {
     successHandle(res, todos);
-  }
-  else if (req.method == "POST" && req.url == "/todo") {
-    req.on('end', () => {
+  } else if (req.method == "POST" && req.url == "/todo") {
+    req.on("end", () => {
       try {
         const title = JSON.parse(body).title;
         if (title !== undefined) {
           const todo = {
-            "title": title,
-            "id": uuidv4()
+            title: title,
+            id: uuidv4(),
           };
           todos.push(todo);
           successHandle(res, todos);
-        }
-        else {
+        } else {
           errorHandle(res, "沒有title");
         }
-
-      }
-      catch {
+      } catch {
         errorHandle(res, "格式錯誤");
       }
-    })
-  }
-  else if (req.method == "DELETE" && req.url == "/todo") {
+    });
+  } else if (req.method == "DELETE" && req.url == "/todo") {
     todos.length = 0;
     successHandle(res, todos, "刪除成功");
-  }
-  else if (req.method == "DELETE" && req.url.startsWith("/todo?")) {
+  } else if (req.method == "DELETE" && req.url.startsWith("/todo?")) {
     const url = new URL(req.url, `http://${req.headers.host}`);
-    const title = url.searchParams.get('title');
-    const id = url.searchParams.get('id');
+    const title = url.searchParams.get("title");
+    const id = url.searchParams.get("id");
     const beforeLength = todos.length;
-    todos = todos.filter(item => item.id != id);
-    todos = todos.filter(item => item.title != title);
+    todos = todos.filter((item) => item.id != id);
+    todos = todos.filter((item) => item.title != title);
     const afterLength = todos.length;
     if (beforeLength > afterLength) {
       successHandle(res, todos, "刪除成功");
@@ -87,46 +80,41 @@ const requestListener = (req, res) => {
     else {
       errorHandle(res, "刪除失敗");
     } */
-
-  }
-  else if (req.method == "PATCH" && req.url.startsWith("/todo")) {
-    req.on('end', () => {
+  } else if (req.method == "PATCH" && req.url.startsWith("/todo")) {
+    req.on("end", () => {
       try {
-        const url = new URL(req.url, `http://${req.headers.host}`);
-        const id = url.searchParams.get('id');
+        const url = new URL(req.url, `${req.headers.host}`);
+        const id = url.searchParams.get("id");
         const title = JSON.parse(body).title;
-        const index = todos.findIndex(item => item.id == id);
+        const index = todos.findIndex((item) => item.id == id);
         if (title !== undefined && index > -1) {
           todos[index].title = title;
           res.writeHead(200, headers);
-          res.write(JSON.stringify({
-            "url": url,
-            "title": title,
-            "index": index,
-            "message": "更新資料成功",
-            "todos": todos
-          }))
+          res.write(
+            JSON.stringify({
+              url: url,
+              title: title,
+              index: index,
+              message: "更新資料成功",
+              todos: todos,
+            })
+          );
           res.end();
-        }
-        else {
+        } else {
           errorHandle(res, "內容錯誤");
         }
-      }
-      catch {
+      } catch {
         errorHandle(res, "格式錯誤");
       }
-
-    })
-  }
-  else if (req.method == "OPTIONS" && req.url == "/todo") {
+    });
+  } else if (req.method == "OPTIONS" && req.url == "/todo") {
     res.writeHead(200, headers);
     res.end();
-  }
-  else {
+  } else {
     res.writeHead(404, headers);
-    res.end(JSON.stringify({ "status": "fail", "message": "Not Found" }));
+    res.end(JSON.stringify({ status: "fail", message: "Not Found" }));
   }
-}
+};
 
 const server = http.createServer(requestListener);
 const port = process.env.PORT || 3005;
@@ -138,19 +126,18 @@ server.listen(port, () => {
 // 處理應用程序退出信號
 const shutdown = () => {
   server.close(() => {
-    console.log('Process terminated');
+    console.log("Process terminated");
     process.exit(0);
   });
 };
 
-process.on('SIGTERM', shutdown);
-process.on('SIGINT', shutdown);
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
 
-server.on('error', (e) => {
-  if (e.code === 'EADDRINUSE') {
+server.on("error", (e) => {
+  if (e.code === "EADDRINUSE") {
     console.error(`Port ${port} is already in use`);
   } else {
     console.error(`Server error: ${e}`);
   }
 });
-
